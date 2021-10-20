24540ef80952a219740f949c8d40ca82c5e6d118
QuQ-bb <quqsue@gmail.com>
2021-10-20 15:46:09 +0900

    Membercontroller.java 확장자 패턴(.member)로 변경

--- src/main/java/controller/membercontroller.java
+++ src/main/java/controller/membercontroller.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.io.IOException;
 
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,55 +15,65 @@ import service.ActionForward;
 import service.Idcheck;
 import service.Login;
 import service.Member;
+import service.Nickcheck;
 
 /**
  * Servlet implementation class board_controller
  */
-@WebServlet("*.do")
-public class membercontroller extends HttpServlet {
+@WebServlet("*.member")
+public class Membercontroller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	// doGet(), doPost() 메소드에서 공통적인 작업을 처리하는 메소드
-	public void doProcess(HttpServletRequest request, HttpServletResponse response) {
+	public void doProcess(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		
 		String requestURI = request.getRequestURI();
 		String contextPath = request.getContextPath();
 		String command = requestURI.substring(contextPath.length());
 		
-		System.out.println("requestURI"+requestURI);
-		System.out.println("contextPath"+contextPath);
-		System.out.println("command"+command);
+		System.out.println("requestURI:"+requestURI);
+		System.out.println("contextPath:"+contextPath);
+		System.out.println("command:"+command);
 		
 		Action action = null;
 		ActionForward forward = null;
 		
 		//회원 가입
-		if(command.equals("/Member.do")) {
+		if(command.equals("/Member.member")) {
 			try {
 				action = new Member();
-				forward =action.execute(request,response);
+				forward = action.execute(request,response);
 				}catch(Exception e) {
 					e.printStackTrace();
 				}
 					
 			//ID중복 검사(ajax)	
-		}else if(command.equals("/Idcheck.do")) {
+		}else if(command.equals("/Idcheck.member")) {
 			try {
 				action = new Idcheck();
 				forward = action.execute(request, response);
 			}catch(Exception e) {
 				e.printStackTrace();
 			}
+		
+		//닉네임 중복검사(ajax)
+		}else if(command.equals("/Nickcheck.member")) {
+			try {
+				action = new Nickcheck();
+				forward = action.execute(request, response);
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
 					
 		//회원 가입 폼
-		}else if(command.contentEquals("/Join.do")) {
+		}else if(command.contentEquals("/Join.member")) {
 			forward = new ActionForward();
 			forward.setRedirect(false);
 			forward.setPath("./member/join.jsp");
 			
 					
 		//로그인(회원인증)	
-		}else if(command.equals("/Login.do")) {
+		}else if(command.equals("/Login.member")) {
 			try {
 				action = new Login();
 				forward = action.execute(request, response);
@@ -71,21 +82,21 @@ public class membercontroller extends HttpServlet {
 			}
 					
 		//로그인 폼	
-		}else if(command.equals("/Login.do")){
+		}else if(command.equals("/Login.member")){
 			forward = new ActionForward();
 			forward.setRedirect(false);
-			forward.setPath("/member/login.jsp");
+			forward.setPath("./member/login.jsp");
+			
 		
 		//로그아웃	
-		}else if(command.equals("/Logout.do")){
+		}else if(command.equals("/Logout.member")){
 			forward = new ActionForward();
 			forward.setRedirect(false);
 			forward.setPath("/member/logout.jsp");	
 	
-		}//임시로 박아둔거
-		
-			//회원가입 이메일인증
-/*		}else if(command.equals("/.do")) {
+		}
+			//회원가입 프로필사진
+	/*	}else if(command.equals("/.member")) {
 			
 			try {
 				action = new ;
@@ -94,8 +105,21 @@ public class membercontroller extends HttpServlet {
 				e.printStackTrace();
 			}
 		}
+		
 		*/
 		
+		//회원가입 이메일인증
+		/*	}else if(command.equals("/.member")) {
+			
+			try {
+				action = new ;
+				forword = action.execute(request, response);		
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		
+		 */
 		
 		
 		
@@ -111,21 +135,18 @@ public class membercontroller extends HttpServlet {
 		
 		
 		
+		//포워딩 처리
+		if(forward != null) {
+			if(forward.isRedirect()) {	//redirect 방식으로 포워딩
+				response.sendRedirect(forward.getPath());
+			}else {						//dispatcher 방식으로 포워딩
+				RequestDispatcher dispatcher =
+						request.getRequestDispatcher(forward.getPath());
+				dispatcher.forward(request, response);
+			}
+		}
 		
-		
-		
-		
-		
-		
-		
-		
-		  //포워딩처리 if(forward != null) { if(forward.isRedirect()) { // redirect 방식으로 포워딩
-		  /*response.sendRedirect(forward.getPath()); }else { // dispatcher 방식으로 포워딩
-		  RequestDispatcher dispatcher =
-		  request.getRequestDispatcher(forward.getPath()); dispatcher.forward(request,
-		  response); } }*/
-		 
-	} // doProcess() end
+	}	//doProcess() end
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
